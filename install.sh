36742ede4b09a1cceb69570161b72a2c8d977245
Jason <git-admin@casjaysdev.com>
2022-07-04 09:26:26 -0400

    🗃️  modified: install.sh 🗃️

--- install.sh
+++ install.sh
@@ -112,7 +112,7 @@ install_cpan "$CPAN"
 install_gem "$GEMS"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Other dependencies
-dotfilesreq git misc
+dotfilesreq
 dotfilesreqadmin
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Ensure directories exist
